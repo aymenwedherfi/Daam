09cd798fa327716178fbb9872238c00c47873bcb
SimonePasetto <simone.pasetto@amilon.eu>
2023-05-23 09:54:04 +0200

    install bootstrap 5

--- Simay.Daam.Web/src/main.js
+++ Simay.Daam.Web/src/main.js
@@ -1,10 +1,12 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import './assets/main.css'
 
 const app = createApp(App)
 
